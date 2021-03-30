bcf4caac914d512e690f3a1a55e9e8200684c99f
jgerardo214 <gerardo.jon@gmail.com>
2021-03-29 23:24:56 -0700

    Added Students Table VC

---
+++ On the Map/Controller/StudentsTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StudentsTableViewController.swift
+//  On the Map
+//
+//  Created by Jonathan Gerardo on 3/29/21.
+//
+
+import Foundation
+import UIKit
+
+struct StudentsTableViewController {
+    
+    var results = [StudentInformation]()
+    
+    
+    
+}
